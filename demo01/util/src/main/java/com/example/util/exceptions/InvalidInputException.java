09ea92602058b2662e654b4e051f90936288a1bb
Hoai Nguyen <hn003f@att.com>
2020-03-29 19:38:49 -0700

    01 - Creating a Set of Cooperating Microservices

---
+++ demo01/util/src/main/java/com/example/util/exceptions/InvalidInputException.java
@@ -0,0 +1,19 @@
+package com.example.util.exceptions;
+
+public class InvalidInputException extends RuntimeException {
+	private static final long serialVersionUID = 2639687325711549213L;
+
+	public InvalidInputException() {}
+
+	public InvalidInputException(String message) {
+		super(message);
+	}
+
+	public InvalidInputException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public InvalidInputException(Throwable cause) {
+		super(cause);
+	}
+}
---
+++ demo01/util/src/main/java/com/example/util/exceptions/NotFoundException.java
@@ -0,0 +1,19 @@
+package com.example.util.exceptions;
+
+public class NotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 5588556785365899332L;
+
+	public NotFoundException() {}
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
